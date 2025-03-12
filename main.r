f083b026ab4243f703c667860726cda3a61d59d8
Ben Stidham <ben.stidham1@gmail.com>
2025-03-12 15:13:37 -0500

    some data stuff

--- main.r
+++ main.r
@@ -5,3 +5,11 @@ winter_olympics <- read.csv("Athletes_summer_games.csv", header = TRUE)
 regions <- read.csv("regions.csv", header = TRUE)
 global_ses <- read.csv("GLOB.SES.csv", header = TRUE)
 
+olympics <- merge(summer_olympics, winter_olympics)
+olympics <- olympics[which(olympics$Sport == "Swimming"), ]
+olympics$Medal[which(olympics$Medal == "")] <-NA 
+
+# Plotting Year vs Age (in base graphics) where people won a medal
+ggplot(data = olympics[!is.na(olympics$Medal), ]) +
+  geom_point(mapping = aes(x = Year, y = Age, color = as.factor(Medal))) + 
+  labs(x = "Year", y = "Age", title = "Olympics Medal Winners")
