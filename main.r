c99f434f8d604112c249f91cbd55fed8e58978fb
Ben Stidham <ben.stidham1@gmail.com>
2025-03-12 10:14:15 -0500

    updated main.r. Can read in our csv files

--- main.r
+++ main.r
@@ -1,1 +1,7 @@
 library("ggplot2")
+
+summer_olympics <- read.csv("Athletes_summer_games.csv", header = TRUE)
+winter_olympics <- read.csv("Athletes_summer_games.csv", header = TRUE)
+regions <- read.csv("regions.csv", header = TRUE)
+global_ses <- read.csv("GLOB.SES.csv", header = TRUE)
+
