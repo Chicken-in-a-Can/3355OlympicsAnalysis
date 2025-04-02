932b4b32f1d2163674fcb3033eb317c8247a7c54
Ananya-Sankaran <jayagurudattaladdu@gmail.com>
2025-04-02 15:52:53 -0500

    Update main.r

--- main.r
+++ main.r
@@ -15,3 +15,20 @@ ggplot(data = olympics[!is.na(olympics$Medal), ]) +
   geom_point(mapping = aes(x = Year, y = Age, color = as.factor(Medal))) + 
   scale_color_manual(values = c("#CD7F32", "#C0C0C0", "#FFD700")) + 
   labs(x = "Competition Year", y = "Athlete Age", title = "Olympics Medal Winners", color = "Medal Won")
+
+#mean/median age for each year
+summer <- read.csv("Athletes_summer_games.csv")                                 
+socioeconomic <- read.csv("GLOB.SES.csv")
+subset_swim <- summer[which(summer[, "Sport"] == "Swimming"), ]
+
+unique_years <- sort(unique(subset_swim[, "Year"]))
+mean_ages <- NULL
+median_ages <- NULL
+
+for (i in unique_years) {
+  print(i)
+  mean_ages <- c(ages, mean(subset_swim[which(subset_swim[, "Year"] == i), "Age"], na.rm = TRUE))
+  median_ages <- c(ages, median(subset_swim[which(subset_swim[, "Year"] == i), "Age"], na.rm = TRUE))
+}
+mean_ages
+median_ages
